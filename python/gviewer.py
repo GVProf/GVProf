401e9618227610a1aeecd252cc9417d8423b563e
Jokeren <robinho364@gmail.com>
2020-11-07 21:23:42 -0600

    Update visualization script

--- python/gviewer.py
+++ python/gviewer.py
@@ -29,22 +29,30 @@ def format_context(context, choice, known, leaf):
   return ret
 
 
-def create_graph(args):
+def format_graph(args):
     file_path = args.file
     G = pgv.AGraph(file_path)
-    G.node_attr['shape'] = 'record'
 
+    for node in G.nodes():
+        for key, value in node.attr.items():
+            if key == 'context':
+                value = format_context(
+                    value, args.context_filter, args.known, args.leaf)
+                node.attr['context'] = value
+
+    return G
+
+
+def create_plain_graph(G):
     for node in G.nodes():
         name = node.get_name()
         label = '{'
         label += '<name> ' + name + '|'
         for key, value in node.attr.items():
-            if key == 'context':
-                value = format_context(
-                    value, args.context_filter, args.known, args.leaf)
             label += '{<' + key + '> ' + key.upper() + '|' + value + '}|'
         label = label[:-1]
         label += '}'
+        node.attr['shape'] = 'record'
         node.attr['label'] = label
 
     for edge in G.edges():
@@ -59,6 +67,27 @@ def create_graph(args):
     return G
 
 
+def create_pretty_graph(G):
+    G.graph_attr['bgcolor'] = '#2e3e56'
+    G.graph_attr['pad'] = '0.5'
+
+    for node in G.nodes():
+        node.attr['shape'] = 'circle'
+        node.attr['width'] = '0.6'
+        node.attr['style'] = 'filled'
+        node.attr['fillcolor'] = '#edad56'
+        node.attr['color'] = '#edad56'
+        node.attr['penwidth'] = '3'
+        node.attr['tooltip'] = node.attr['context'].replace('\l', '&#10;')
+    
+    for edge in G.edges():
+        edge.attr['color'] = '#fcfcfc'
+        edge.attr['pendwidth'] = '2'
+        edge.attr['fontname'] = 'helvetica Neue Ultra Light'
+
+    return G
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file', help='file name')
 parser.add_argument('-cf', '--context-filter', choices=[
@@ -69,8 +98,17 @@ parser.add_argument('-l', '--leaf', action='store_true', default=False,
                     help='show only leaf function')
 parser.add_argument('-of', '--output-format',
                     choices=['svg', 'png', 'pdf'], default='svg', help='output format')
+parser.add_argument('-pr', '--pretty', action='store_true', default=False,
+                    help='tune output graph')
 args = parser.parse_args()
 
-G = create_graph(args)
+G = format_graph(args)
+if args.pretty:
+    G = create_pretty_graph(G)
+else:
+    G = create_plain_graph(G)
+
 G.layout(prog='dot')
+
+#G.write(args.file + '.dot')
 G.draw(args.file + '.' + args.output_format)
